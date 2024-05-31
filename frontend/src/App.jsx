8f5b279b4e5a6dee18376cb5fefdf80cc9e79461
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-05-31 11:25:21 +0545

    Some updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element = { <Home />} />
+      </Routes>
+      <Footer />
+    </Router>
       
     </>
   )
